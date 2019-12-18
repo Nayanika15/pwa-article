163c1921be0e118d724c4936427b17b5fb07de77
nayanika15 <nayanikapanigrahi1115@gmail.com>
2019-12-18 17:11:08 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <v-app>
+      <div class="wrap">
+        <v-overlay v-if="$store.state.home.is_loading">
+          <v-progress-circular primary indeterminate> </v-progress-circular>
+        </v-overlay>
+        <appHeader></appHeader>
+        <router-view></router-view>
+        <appFooter></appFooter>
+      </div>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Header from "./components/common/Header.vue";
+import Footer from "./components/common/Footer.vue";
+export default {
+  components: {
+    appHeader: Header,
+    appFooter: Footer
+  }
+};
+</script>
